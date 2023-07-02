dd6537c3d6516d0bb3fe9a76c62844b62c1f06d6
Aurelien <masiero.aurelien@outlook.fr>
2023-07-03 00:44:15 +0200

    Air03

---
+++ air02.rb
@@ -0,0 +1,30 @@
+# Créez un programme qui transforme un tableau de chaînes de caractères en une seule chaîne de caractères. Espacés d’un séparateur donné en dernier argument au programme.
+
+def concatener(listgroup, separateur)
+  if listgroup.nil? || separateur.nil?
+    puts "Error"
+    return
+  end
+
+  if ARGV.length < 3
+    puts "Error"
+    return
+  end
+
+  result = listgroup.join(separateur)
+
+  return result
+
+#  result.each do |element|
+#    puts element.join
+#  end
+
+end
+
+cli = ARGV
+listgroup = cli.slice(0, cli.length - 1)
+separateur = ARGV[-1].to_s
+
+result = concatener(listgroup, separateur)
+
+puts result
