ecc8cc9fa42479e3109182dd4118a19135be8846
Aurelien <masiero.aurelien@outlook.fr>
2023-07-03 00:57:20 +0200

    Air04

---
+++ air04.rb
@@ -0,0 +1,22 @@
+# Créez un programme qui affiche une chaîne de caractères en évitant les caractères identiques adjacents.
+
+def afficher_sans_adjacents_identiques(chaine)
+  if chaine.empty?
+    puts "Error"
+    return
+  end
+
+  nouvelle_chaine = chaine[0]
+
+  (1...chaine.length).each do |i|
+    if chaine[i] != chaine[i - 1]
+      nouvelle_chaine += chaine[i]
+    end
+  end
+
+  puts nouvelle_chaine
+end
+
+chaine = ARGV.join("")
+
+afficher_sans_adjacents_identiques(chaine)
