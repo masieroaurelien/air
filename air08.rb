b07daa82d25e33d4d9ef9c1366b887eea61681af
Aurelien <masiero.aurelien@outlook.fr>
2023-07-03 02:29:55 +0200

    Air08

---
+++ air08.rb
@@ -0,0 +1,25 @@
+# Créez un programme qui fusionne deux listes d’entiers triées en les gardant triées, les deux listes seront séparées par un “fusion”.
+
+
+def merge_list(arguments)
+  splitter = arguments.index("fusion")
+  splitter_down = splitter - 1
+  splitter_up = splitter + 1
+  list1 = arguments[0..splitter_down]
+  list2 = arguments[splitter_up..]
+  list1 = list1.sort
+  list2 = list2.sort
+
+  if splitter > 0
+    merged_list = list1 + list2
+    result = merged_list.sort.join(' ')
+    puts result
+  else
+    puts "Error"
+    return
+  end
+end
+
+arguments = ARGV
+
+merge_list(arguments)
