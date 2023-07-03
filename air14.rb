745e930d0fd411fbca434f19902663eb10d6e76a
Aurelien <masiero.aurelien@outlook.fr>
2023-07-03 16:46:47 +0200

    Air14: Done

---
+++ air14.rb
@@ -0,0 +1,6 @@
+#Well done !
+
+quotelist = ["plus compliqué encore que l'eau par moments", "sympa", "parfois plutôt facile, parfois pas du tout du tout"]
+quote = quotelist.sample
+
+puts "J'ai terminé l'épreuve de l'Eau et c'était #{quote}!"
